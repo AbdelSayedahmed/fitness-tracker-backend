fef80781c370dda26e5004519102f98edf847471
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-06-29 16:41:49 -0400

    error handling for get workout with id

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const workouts = require("../models/workouts");
 
 router.get("/", (res, req) => {
-  res.statusCode(200).send(workouts);
+  res.status(200).send(workouts);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const workout = workouts.find((input) => input.id === +id);
+  workout
+    ? res.statusCode(200).send(workout)
+    : res.status(404).json({ error: `Workout with id: ${id} not found!` });
 });
 
 module.exports = router;
